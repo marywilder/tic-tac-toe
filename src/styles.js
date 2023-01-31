c6e41a817f2965c9e61065843d2f91b2f520788d
Mary <marywilder717@gmail.com>
2023-01-31 15:27:44 -0500

    first commit

---
+++ src/styles.js
@@ -0,0 +1,19 @@
+const ocolor = '#eafc88';
+const height = '100vh';
+const flex = 'flex';
+const center = 'center';
+const ycenter = 'center';
+const font = 'Lilita One, cursive';
+
+export const style = {
+    
+    height: height,
+    display: flex,
+    justifyContent: center,
+    alignItems: center,
+    fontFamily: font,
+}
+
+export const color = {
+    color: ocolor
+}
